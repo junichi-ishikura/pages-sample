15f5174b50aa2ad682907ba746b6dad10c9f961f
Junichi Ishikura <ishikura.junichi@mnmleng.com>
2023-12-09 16:45:18 +0900

    working example

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export const config = {
+  runtime: 'edge',
+}
+
+export default async function (req) {
+  return Response.json({ name: 'John Doe' })
 }
